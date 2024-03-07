06cd577aa3dada3c1a70d75f196c19ad9fc7f7b7
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-07 08:17:52 +0100

    pack_web_static.py reworked

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,22 +4,15 @@ the contents of the web_static folder"""
 from datetime import datetime
 from fabric.api import *
 
-env.user = 'ubuntu'
-env.hosts = ['34.204.81.235']
-env.key_filename = "~/.ssh/id_rsa_alx"
-
-
-def create_folder():
-    """Creates a folder named versions"""
-    local('mkdir -p versions')
-
 
 def do_pack():
     """generates a .tgz archive from the
     contents of the web_static folder"""
     tp = datetime.now()
-    filename = "web_static_{}.tgz".format()
-    create_folder()
+    filename = "web_static_{}.tgz".format(tp.strftime("%Y%m%d%H%M%S"))
+    res = local('mkdir -p versions', capture=True)
+    if res.return_code != 0:
+        return None
     res = local("tar -czf versions/{} ./web_static".format(filename),
                 capture=True)
     if res.return_code != 0:
