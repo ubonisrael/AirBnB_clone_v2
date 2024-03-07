f00fda26f779906242d19d6e932440972a8d4095
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-07 08:41:38 +0100

    do_deploy_web_static.py reworked

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,9 +7,9 @@ from fabric.api import *
 from os import path
 import re
 
-env.user = 'ubuntu'
+# env.user = 'ubuntu'
 env.hosts = ['34.204.81.235', '18.234.107.45']
-env.key_filename = "~/.ssh/id_rsa_alx"
+# env.key_filename = "~/.ssh/id_rsa_alx"
 
 
 def do_deploy(archive_path):
@@ -18,25 +18,45 @@ def do_deploy(archive_path):
     #     return False
     if not path.exists(archive_path):
         return False
-    
-    put(archive_path, "/tmp/")
+
+    if put(archive_path, "/tmp/").failed is True:
+        return None
     # get the archive filename without extension
     pattern = r'\/([^\/.]+)\.'
     match = re.search(pattern, archive_path)
     pathname = match.group(1)
+    # get filename with extension
+    fn = archive_path.split('/')[1]
     # create destination folder for unzipping
     destination_path = "/data/web_static/releases/{}".format(pathname)
-    run("mkdir -p {}".format(destination_path))
+    cmd = run("mkdir -p {}".format(destination_path))
+    if cmd.failed is True:
+        return None
     # unzip files to destination folder
-    run("tar -xvzf /tmp/{} -C /data/web_static/releases/{}".format(
-        archive_path.split('/')[1], pathname))
+    cmd = run("tar -xvzf /tmp/{} -C /data/web_static/releases/{}".format(
+         fn, pathname))
+    if cmd.failed is True:
+        return None
     # delete archive
-    sudo("rm -rf /tmp/{}".format(archive_path.split('/')[1]))
+    cmd = sudo("rm -rf /tmp/{}".format(fn))
+    if cmd.failed is True:
+        return None
     # delete old symbolic link
-    run("rm -rf /data/web_static/current")
+    cmd = run("rm -rf /data/web_static/current")
+    if cmd.failed is True:
+        return None
     # move all contents of decompressed web_static to
     # destination folder, then delete it
-    run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(pathname, pathname))
-    run("rm -rf /data/web_static/releases/{}/web_static".format(pathname))
+    cmd = run("mv /data/web_static/releases/{}/web_static/*\
+               /data/web_static/releases/{}".format(pathname, pathname))
+    if cmd.failed is True:
+        return None
+    cmd = run("rm -rf /data/web_static/releases/{}/web_static".format(
+        pathname))
+    if cmd.failed is True:
+        return None
     # add new symbolic link
-    run("ln -sf {} /data/web_static/current".format(destination_path))
+    cmd = run("ln -sf {} /data/web_static/current".format(destination_path))
+    if cmd.failed is True:
+        return None
+    return True
