cf7452be4cc34a7f27049db271b260c4cce8c693
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-07 08:50:27 +0100

    deploy_web_static.py reworked

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -11,52 +11,65 @@ env.hosts = ['34.204.81.235', '18.234.107.45']
 env.key_filename = "~/.ssh/id_rsa_alx"
 
 
-def create_folder():
-    """Creates a folder named versions"""
-    local('mkdir -p versions')
-
-
 def do_pack():
     """generates a .tgz archive from the
     contents of the web_static folder"""
     tp = datetime.now()
-    filename = "web_static_{}{}{}{}{}{}.tgz".format(
-        tp.year, tp.month, tp.day, tp.hour, tp.minute, tp.second)
-    create_folder()
+    filename = "web_static_{}.tgz".format(tp.strftime("%Y%m%d%H%M%S"))
+    res = local('mkdir -p versions', capture=True)
+    if res.return_code != 0:
+        return None
     res = local("tar -czf versions/{} ./web_static".format(filename),
                 capture=True)
     if res.return_code != 0:
         return None
     return "versions/{}".format(filename)
 
+
 def deploy():
     """Deploys an archive to web servers"""
-    # if archive_path is None:
-    #     return False
     archive_path = do_pack()
     if not path.exists(archive_path):
         return False
-    
-    put(archive_path, "/tmp/")
+
+    if put(archive_path, "/tmp/").failed is True:
+        return None
     # get the archive filename without extension
     pattern = r'\/([^\/.]+)\.'
     match = re.search(pattern, archive_path)
     pathname = match.group(1)
+    # get filename with extension
+    fn = archive_path.split('/')[1]
     # create destination folder for unzipping
     destination_path = "/data/web_static/releases/{}".format(pathname)
-    run("mkdir -p {}".format(destination_path))
+    cmd = run("mkdir -p {}".format(destination_path))
+    if cmd.failed is True:
+        return None
     # unzip files to destination folder
-    run("tar -xvzf /tmp/{} -C /data/web_static/releases/{}".format(
-        archive_path.split('/')[1], pathname))
+    cmd = run("tar -xvzf /tmp/{} -C /data/web_static/releases/{}".format(
+         fn, pathname))
+    if cmd.failed is True:
+        return None
     # delete archive
-    sudo("rm -rf /tmp/{}".format(archive_path.split('/')[1]))
+    cmd = sudo("rm -rf /tmp/{}".format(fn))
+    if cmd.failed is True:
+        return None
     # delete old symbolic link
-    run("rm -rf /data/web_static/current")
+    cmd = run("rm -rf /data/web_static/current")
+    if cmd.failed is True:
+        return None
     # move all contents of decompressed web_static to
     # destination folder, then delete it
-    run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(pathname, pathname))
-    run("rm -rf /data/web_static/releases/{}/web_static".format(pathname))
+    cmd = run("mv /data/web_static/releases/{}/web_static/*\
+               /data/web_static/releases/{}".format(pathname, pathname))
+    if cmd.failed is True:
+        return None
+    cmd = run("rm -rf /data/web_static/releases/{}/web_static".format(
+        pathname))
+    if cmd.failed is True:
+        return None
     # add new symbolic link
-    run("ln -sf {} /data/web_static/current".format(destination_path))
-
+    cmd = run("ln -sf {} /data/web_static/current".format(destination_path))
+    if cmd.failed is True:
+        return None
     return True
