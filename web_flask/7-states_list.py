e16b411d9b231e5b50b94d4ee1354f15372476bd
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-23 10:02:29 +0100

    added whitespace trim for task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template
 from models import storage
 
 app = Flask(__name__)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
