864b7cd4700eaf525f737e7c7001235d83aed9b9
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-23 09:58:18 +0100

    fixed typo in html template

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,8 +4,6 @@ from flask import Flask, render_template
 from models import storage
 
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
