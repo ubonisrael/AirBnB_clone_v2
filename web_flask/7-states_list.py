2e19e9144d7ce41e2adc70d2592559ee31710b1c
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-23 18:02:02 +0100

    moved teardown_appcontext below route

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,17 +6,17 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """closes the connection to the database"""
-    storage.close()
-
-
 @app.route("/states_list", strict_slashes=False)
 def states_list_page():
     """displays a page containing states"""
     states_list = storage.all("State").values()
     return render_template('7-states_list.html', states_list=states_list)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the connection to the database"""
+    storage.close()
 
 
 if __name__ == "__main__":
