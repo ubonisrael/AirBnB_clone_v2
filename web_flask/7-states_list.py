42dc4ac1df920fc79df064dbbabb7428f2e5e898
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-23 10:14:43 +0100

    manully added host

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,11 +15,9 @@ def teardown_db(exception):
 @app.route("/states_list", strict_slashes=False)
 def states_list_page():
     """displays a page containing states"""
-    states_list = []
-    for item in storage.all("State").values():
-        states_list.append(item)
+    states_list = storage.all("State").values()
     return render_template('7-states_list.html', states_list=states_list)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
