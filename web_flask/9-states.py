bdf85dde3db1607dfa8d489b374062a942cbcf63
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-23 19:36:44 +0100

    new trial - 5

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -2,7 +2,6 @@
 """starts a flask application"""
 from flask import Flask, render_template
 from models import storage
-from models import storage
 from models.state import State
 
 app = Flask(__name__)
