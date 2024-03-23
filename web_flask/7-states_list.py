ca7de2e58f305420929f9012b81d609fad191992
ubonisrael <akpanudohubonisrael@gmail.com>
2024-03-23 19:08:16 +0100

    new trial

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states_list_page():
     """displays a page containing states"""
-    states_list = storage.all(State).values()
+    states_list = storage.all(State)
     return render_template('7-states_list.html', states_list=states_list)
 
 
